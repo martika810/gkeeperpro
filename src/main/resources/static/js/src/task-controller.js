1bfce1d2a460c3d2c374946240975b79d170eaee
1984MRB <fraguel_rock22@hotmail.com>
2017-06-14 23:27:58 +0100

    Added comments

--- src/main/resources/static/js/src/task-controller.js
+++ src/main/resources/static/js/src/task-controller.js
@@ -2,16 +2,17 @@ console.log("Hello offer");
 var taskApp = angular.module('taskApp',[]);
 taskApp.controller('taskController',function($scope,$http){
 	
+	//Esto hace una llamada a la clase TaskController
+	//Los datos de respuseta se devuelven en "response.data"
+	//Y se asignan a la variable taskList
 	$http.get("/task")
 			.then(function(response){
 				$scope.taskList = response.data
 				
+				//cuando haya un cambio en la variable "taskList", entonces se ejecuta estaa funcion
 				$scope.$watch('taskList', function() {
 					 $('.collapsible').collapsible();
 				}, true);
 			});
-	
-	
-
-	
+		
 });

79e6888b429418a7fec12f391514783e477f615b
1984MRB <fraguel_rock22@hotmail.com>
2017-07-03 09:12:04 +0100

    in progress

--- src/main/resources/static/js/src/employee-controller.js
+++ src/main/resources/static/js/src/employee-controller.js
@@ -1,37 +1,52 @@
 console.log("Hello employee-controller");
 var employeeApp = angular.module('employeeApp',[]);
-employeeApp.controller('employeeController',function($scope,$http){
+
+employeeApp.service('employeeService',function($scope,$http,$q){
+    return{
+	loadTaskForAnEmployee:function(taskGroupByEmployeeMap,taskMap){
+	    var taskByEmployee = {};
+	    for (var employeeId in taskGroupByEmployeeMap ) {
+		 var taskList = [];
+		 taskGroupByEmployeeMap[employeeId].forEach(function(entry){
+		     taskList.push(taskMap[entry]);
+		 });
+		 taskList;
+		 taskByEmployee[employeeId]=taskList;
+	    }
+	    return taskByEmployee;
+	},
+        loadData:function(){
+  	  var taskRequest = $http.get("/task")
+  	   var employeeRequest = $http.get("/employee");
+  	   var taskBrupByEmployeeRequest =  $http.get("task/by_employee");
+  	   var toolRequest = $http.get("/tool");
+  	   $q.all([taskRequest,employeeRequest,taskBrupByEmployeeRequest,toolRequest])
+  	   .then(function(response){
+  	       $('.collapsible').collapsible();
+  	       $scope.taskMap = response[0].data;
+  	       $scope.employeeMap = response[1].data;
+  	       $scope.taskGroupByEmployeeMap = response[2].data;
+  	       $scope.toolMap = response[3].data;
+               $scope.taskGroupByEmployeeMap = $scope.loadTaskForAnEmployee($scope.taskGroupByEmployeeMap,$scope.taskMap);
+  	       
+  	   });
+        }
+    } 
+});
+employeeApp.controller('employeeController',function(employeeService){
 	
 	//Esto hace una llamada a la clase TaskController
 	//Los datos de respuseta se devuelven en "response.data"
 	//Y se asignan a la variable employeeList
-
-
-	$scope.populateEmployeePanel = function(){
-	    $http.get("/employee")
-    	    .then(function(response){
-    			$scope.employeeList = response.data
-
-    			//cuando haya un cambio en la variable "employeeList", entonces se ejecuta estaa funcion
-    		    $scope.$watch('employeeList', function() {
-    				 $('.collapsible').collapsible();
-    		}, true);
-    	});
-
-    	$http.get("/task")
-    	    .then(function(response){
-    	        $scope.taskMap = response.data
-    	    });
-
-    	$http.get("task/by_employee")
-    	    .then(function(response){
-                $scope.taskGroupByEmployeeMap = response.data
-    	    });
-	};
-
-	$scope.populateEmployeePanel();
+    
+       $scope.save = function(){
+	    console.log("Click save");
+	}
+	employeeService.loadData();
 		
 });
+
+
 
 
 angular.element(document).ready(function() {

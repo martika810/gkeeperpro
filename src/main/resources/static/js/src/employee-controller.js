760d88fbdab96ab1378f4d50db78e494b4d65f01
1984MRB <fraguel_rock22@hotmail.com>
2017-07-04 00:24:22 +0100

    Added task container in employee panel

--- src/main/resources/static/js/src/employee-controller.js
+++ src/main/resources/static/js/src/employee-controller.js
@@ -1,39 +1,55 @@
 console.log("Hello employee-controller");
 var employeeApp = angular.module('employeeApp',[]);
-
-employeeApp.service('employeeService',function($scope,$http,$q){
-    return{
-	loadTaskForAnEmployee:function(taskGroupByEmployeeMap,taskMap){
-	    var taskByEmployee = {};
+employeeApp.factory("employeeService",['$rootScope','$http','$q',function($rootScope,$http,$q){
+    
+    function loadTaskForAnEmployee(taskGroupByEmployeeMap,taskMap){
+	var taskByEmployee = {};
 	    for (var employeeId in taskGroupByEmployeeMap ) {
 		 var taskList = [];
 		 taskGroupByEmployeeMap[employeeId].forEach(function(entry){
 		     taskList.push(taskMap[entry]);
 		 });
-		 taskList;
+		 
 		 taskByEmployee[employeeId]=taskList;
 	    }
 	    return taskByEmployee;
+    }
+    function loadEmployee(){
+	var employeeResponse = {};
+	$http.get("/employee").then(function(response){
+	    $rootScope.employeeMap= response.data;
+	});
+	
+		
+    }
+    return{
+	loadData:function(){
+	    var taskGroupByEmployeeMap = {};
+	    //var employeeRequest = $http.get("/employee");
+	    var taskGroupByEmployeeRequest = $http.get("task/by_employee");
+	    //var toolRequest = $http.get("/tool");
+	    var taskRequest = $http.get("/task");
+	    
+	    $q.all([taskRequest,taskGroupByEmployeeRequest])
+	    	.then(function(response){
+	    	    $('.collapsible').collapsible();
+	    	    var taskMap = response[0].data;
+	    	   // var employeeMap = response[1].data;
+	    	   // taskGroupByEmployeeMap = response[1].data;
+	    	   // var toolMap = response[3].data;
+	    	  $rootScope.taskGroupByEmployeeMap=loadTaskForAnEmployee(response[1].data,taskMap);
+	    	   
+	    	});
+	    
+	    
 	},
-        loadData:function(){
-  	  var taskRequest = $http.get("/task")
-  	   var employeeRequest = $http.get("/employee");
-  	   var taskBrupByEmployeeRequest =  $http.get("task/by_employee");
-  	   var toolRequest = $http.get("/tool");
-  	   $q.all([taskRequest,employeeRequest,taskBrupByEmployeeRequest,toolRequest])
-  	   .then(function(response){
-  	       $('.collapsible').collapsible();
-  	       $scope.taskMap = response[0].data;
-  	       $scope.employeeMap = response[1].data;
-  	       $scope.taskGroupByEmployeeMap = response[2].data;
-  	       $scope.toolMap = response[3].data;
-               $scope.taskGroupByEmployeeMap = $scope.loadTaskForAnEmployee($scope.taskGroupByEmployeeMap,$scope.taskMap);
-  	       
-  	   });
-        }
-    } 
-});
-employeeApp.controller('employeeController',function(employeeService){
+	loadEmployee:function(){
+	    loadEmployee();
+	}
+    }
+}]);
+
+employeeApp.controller('employeeController',['$scope','employeeService',function($scope,employeeService){
 	
 	//Esto hace una llamada a la clase TaskController
 	//Los datos de respuseta se devuelven en "response.data"
@@ -41,12 +57,13 @@ employeeApp.controller('employeeController',function(employeeService){
     
        $scope.save = function(){
 	    console.log("Click save");
-	}
-	employeeService.loadData();
+       }
+       employeeService.loadEmployee();
+       employeeService.loadData();
+       
+	
 		
-});
-
-
+}]);
 
 
 angular.element(document).ready(function() {
